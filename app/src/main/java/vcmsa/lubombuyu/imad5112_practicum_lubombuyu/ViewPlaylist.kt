cee2e0fb6885537cc9adfbe188112906fee74f92
LUBOMBUYU <lubombuyu@gmail.com>
2025-06-19 11:14:06 +0200

    Second user interface

--- app/src/main/java/vcmsa/lubombuyu/imad5112_practicum_lubombuyu/ViewPlaylist.kt
+++ app/src/main/java/vcmsa/lubombuyu/imad5112_practicum_lubombuyu/ViewPlaylist.kt
@@ -1,6 +1,8 @@
 package vcmsa.lubombuyu.imad5112_practicum_lubombuyu
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,16 @@ class ViewPlaylist : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Code starts here
+        val mainMenu =  findViewById<ImageButton>(R.id.imgBMainMenu)
+
+
+        // Sending the code to Main menu
+        mainMenu.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
